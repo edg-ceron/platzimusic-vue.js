5aaa29e8a209176a2a4c607da7f38258f12c79d7
Edgar Ceron <edgarceron@MacBook-Air-de-Edgar-2.local>
2018-03-19 21:13:30 -0600

    Se añade carpeta dist

---
+++ src/components/artist.vue
@@ -0,0 +1,25 @@
+<template>
+<li>
+  <img :src='artist.image[2]["#text"]' alt="">
+  <h2>
+    <a :href='artist.url' target='_blank'>{{artist.name}}</a>
+  </h2>
+</li>
+</template>
+
+<script>
+
+export default {
+  name: 'artist',
+  props: ['artist']
+}
+</script>
+
+<style scoped>
+  li {
+    margin: 5px;
+    padding: 10px;
+    border: 1px solid #2c3e5094;
+    border-radius: 5px;
+  }
+</style>
