cf58b15e53dc2517dc96a16ed3cd955b5eb81332
Edgar Ceron <edgarceron@MacBook-Air-de-Edgar-2.local>
2018-03-19 20:46:02 -0600

    Se cambia la funcionalidad para que desde un select puedas elegir el pais, y con esto renderizar la lista de artistas segun esto

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,53 @@
   <div id="app">
     <img src="./assets/logo.png">
     <h1>Platimusic</h1>
+    <select v-model="selectedCountry">
+      <option v-for="country in countries" v-bind:value="country.value">{{country.name}}</option>
+    </select>
     <ul>
-      <li v-for='artist in artists'> {{artist.name}}</li>
+      <Artist v-for='artist in artists' v-bind:artist="artist" v-bind:key='artist.mbid'></Artist>
     </ul>
   </div>
 </template>
 
 <script>
+import Artist from './components/artist'
 import getArtists from './api/index'
 
 export default {
   name: 'app',
   data () {
     return {
-      artists: []
+      artists: [],
+      countries: [
+        {name: 'Mexico', value:'mexico' },
+        {name: 'colombia', value:'colombia' },
+        {name: 'España', value:'spain' },
+        {name: 'Alemania', value:'germany' },
+        {name: 'Andorra', value:'andorra' },
+      ],
+      selectedCountry: 'mexico'
     }
   },
-  mounted: function () {
-    const _this = this
-    getArtists()
+  components: {
+    Artist
+  },
+  methods: {
+    refreshArtist () {
+      const _this = this
+    getArtists(this.selectedCountry)
       .then(function (artists) {
         _this.artists = artists
       })
+    }
+  },
+  mounted: function () {
+      this.refreshArtist()
+  },
+  watch: {
+    selectedCountry: function () {
+      this.refreshArtist()
+    }
   }
 }
 </script>
@@ -45,6 +70,8 @@ h1, h2 {
 ul {
   list-style-type: none;
   padding: 0;
+  display: flex;
+  background: #8080801a;
 }
 
 li {
