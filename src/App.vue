6a554ed94d5bb04b6c4052b7f5a64462ba908ab5
Edgar Ceron <edgarceron@MacBook-Air-de-Edgar-2.local>
2018-03-19 19:47:03 -0600

    trayendo informacion desde la api last.fm mostrando solo los nombres del los artistas

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>Platimusic</h1>
+    <ul>
+      <li v-for='artist in artists'> {{artist.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import getArtists from './api/index'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists: []
+    }
+  },
+  mounted: function () {
+    const _this = this
+    getArtists()
+      .then(function (artists) {
+        _this.artists = artists
+      })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default {
+  appname: 'platzimusic',
+  apiKey: '69ce87b8e1657928613810edaa210dc2',
+  secret: '9a88b76fadf1b7f2d4f4a6fb2d4d40ea',
+  registeredTo: 'edg-ceron'
+}
